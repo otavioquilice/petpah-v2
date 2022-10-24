0d39e1a49d1c69dc982aec854df9232749753e51
Otavio Desktop <otavio@gtmx.com.br>
2022-10-24 16:11:10 -0300

    Ajustes tela de carrinho

--- resources/views/pages/site/carrinho.blade.php
+++ resources/views/pages/site/carrinho.blade.php
@@ -11,37 +11,47 @@
   
   <main class="main_produtos">
     
-    <div class="carrinho_container">
-      <ul class="produtos_carrinho">
+    <div class="container row col-md-12">
+
+      <ul class="col-md-8">
 
         <h1>Carrinho</h1>
 
         @foreach($produtos as $item_prod)
-        
-        <li class="dados_produtos">
-          <div class="info1">
-            <h3 class="titulo_produto">Ração para cachorro colorido</h3>
-            <img src="{{ asset('media/imagens/img2/prod'.$item_prod->produto_id .'.png') }}" alt="item1">  
-            <p class="produtos-preco">R$ {{ $item_prod->produto->preco[0]->preco}}</p>
-                  
-          </div>
+          <li class="dados_produtos">
+            <div class="info1">
+              <h3 class="titulo_produto">Ração para cachorro colorido</h3>
+              <img src="{{ asset('media/imagens/img2/prod'.$item_prod->produto_id .'.png') }}" alt="item1">  
+              <p class="produtos-preco">R$ {{ $item_prod->produto->preco->where('ativo', 1)->first()->preco}}</p>
+                    
+            </div>
 
-          <div name="select" class="info2">
-            <span class="material-symbols-rounded lixo">delete</span>
-            <input type="number" value="{{$item_prod->quantidade}}" class="quantidade_produto"/> 
-            <select class="tipo_produto">
-              <option value="valor3" selected>Selecione o proposito dessa compra</option>
-              <option value="valor2">Consumo proprio</option>
-              <option value="valor1">Doação para ONGs</option>
-             
-            </select>
-            
-          </div>
+            <div name="select" class="info2">
+              {{-- <span class="count_produto_id_{{$item_prod->produto_id}}"> {{ $item_prod->quantidade }} </span>  --}}
+              <h5 id="count_produto_id_{{$item_prod->produto_id}}"> {{ $item_prod->quantidade }} Iten(s) adicionado(s)</h5>
+              <a type="button" class="plus-circle- add-produto-carrinho" data-produto-id="{{ $item_prod->produto_id }}">
+                  <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-plus-circle" viewBox="0 0 16 16">
+                      <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"/>
+                      <path d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4z"/>
+                  </svg>
+              </a>
+              <a type="button" class="x-circle-fill remove-produto-carrinho" data-produto-id="{{ $item_prod->produto_id }}">
+                  <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-file-minus" viewBox="0 0 16 16">
+                      <path d="M5.5 8a.5.5 0 0 1 .5-.5h4a.5.5 0 0 1 0 1H6a.5.5 0 0 1-.5-.5z"/>
+                      <path d="M4 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h8a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H4zm0 1h8a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1H4a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1z"/>
+                  </svg>
+              </a>
+              <select class="tipo_produto">
+                <option value="valor3" selected>Selecione o proposito dessa compra</option>
+                <option value="valor2">Consumo proprio</option>
+                <option value="valor1">Doação para ONGs</option>
+              
+              </select>
+              
+            </div>
 
-        </li>
+          </li>
         @endforeach
-
-
 
         <li class="opcao_entrega">
           <div class="forma_entrega">
@@ -57,20 +67,23 @@
       </ul> 
     
       
-        <div class="resumo">
-          <h1>Resumo</h1>
-
-          @foreach($produtos as $item_prod)
-            <div class="resumo_produto"> <!-- resumo_compra -->
-              <p class="resumo_produto_titulo">{{ $item_prod->produto->nome}}</p> <!-- resumo_titulo -->
-              <p class="resumo_produto_valor">R$ {{ $item_prod->produto->preco[0]->preco}}</p>
-              
-            </div>
-          @endforeach
-
-          <input type="submit" value="Ir para pagamento" class="bt_pagamento">
-          <input type="submit" value="Continuar comprando" class="bt_contimuar">
-        </div>
+      <div class="col-md-4">
+        @php $total = 0 @endphp
+        <h1>Resumo</h1>
+        @foreach($produtos as $item_prod)
+          <div class="resumo_produto"> <!-- resumo_compra -->
+            <h6 class="resumo_produto_titulo">{{ $item_prod->produto->nome}}</h6> <!-- resumo_titulo -->
+            <h6 class="resumo_produto_valor">{{ $item_prod->quantidade }} X R$ {{ $item_prod->produto->preco->where('ativo', 1)->first()->preco}}</h6>
+          </div>
+          <br>
+          @php $total = $total + ( $item_prod->quantidade * $item_prod->produto->preco->where('ativo', 1)->first()->preco) @endphp
+        @endforeach
+        <h4 class="resumo_valor_total">Total de R$: {{ $total}}</h4>
+        
+        <br>
+        <input type="submit" value="Ir para pagamento" class="bt_pagamento">
+        <input type="submit" value="Continuar comprando" class="bt_contimuar">
+      </div>
       
 
     </div>
